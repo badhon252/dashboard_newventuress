44c8319671aa12122aec7b4ee53b215f0b0de455
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-30 16:29:40 +0600

    added: pagination on membership page

--- src/app/(dashboard)/membership/_components/membership-container.tsx
+++ src/app/(dashboard)/membership/_components/membership-container.tsx
@@ -1,18 +1,36 @@
 "use client";
-
-import { DataTable } from "@/components/ui/data-table";
+// Packages
 import {
   ColumnDef,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { useState } from "react";
+
+// Local imports
+import { DataTable } from "@/components/ui/data-table";
+import PacificPagination from "@/components/ui/PacificPagination";
 import { Membership, membershipList } from "./data";
 import { MembershipColumns } from "./membership-column";
 
 const MembershipContainer = () => {
+  const [currentPage, setCurrentpage] = useState(1);
   return (
     <div>
       <TableContainer columns={MembershipColumns} data={membershipList} />
+      <div className="w-full flex justify-between py-[40px]">
+        <p className="text-[#444444] font-normal text-[16px] leading-[19.2px] ">
+          Showing 1 to 25 in first entries
+        </p>
+
+        <div>
+          <PacificPagination
+            currentPage={currentPage}
+            totalPages={5}
+            onPageChange={(page) => setCurrentpage(page)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
