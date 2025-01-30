21fe552329ee43e50a9df3abfe63f11bc565fee4
Kongkon-79 <kongkonjowarder@gmail.com>
2025-01-30 18:00:28 +0600

    add blog page add

--- src/app/(dashboard)/blogs-management/page.tsx
+++ src/app/(dashboard)/blogs-management/page.tsx
@@ -1,21 +1,10 @@
-"use client"
-import React, { useState } from 'react';
-import BlogManagementFilter from './_components/BlogManagementFilter';
-import BlogManagementHeader from './_components/BlogManagementHeader';
-import BlogManagementContainer from './_components/BlogManagementContainer';
-import AddBlogForm from './_components/AddBlogForm';
+import React from 'react';
+import TableAndFormToggler from './_components/TableAndFormToggler';
 
 const page = () => {
-    const [addBlogForm, setAddBlogForm] = useState(false);
     return (
         <div>
-            <BlogManagementHeader addBlogForm={addBlogForm} setAddBlogForm={setAddBlogForm} />
-            <BlogManagementFilter />
-            {
-                addBlogForm ? <AddBlogForm /> : <BlogManagementContainer />
-            }
-            {/* <BlogManagementContainer />
-            <AddBlogForm /> */}
+            <TableAndFormToggler/>
         </div>
     );
 };
