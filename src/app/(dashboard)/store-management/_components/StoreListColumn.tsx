fa2c28a32d58380782e083faafa3c675c62ac317
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-01 11:55:36 +0600

    store management delete modal add

--- src/app/(dashboard)/store-management/_components/StoreListColumn.tsx
+++ src/app/(dashboard)/store-management/_components/StoreListColumn.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { DemoTableItemsType } from "@/data/StoreListData";
 import { Box } from "lucide-react";
 import { ImInfinite } from "react-icons/im";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
 import Image from "next/image";
+import StoreAction from "./StoreAction";
 
 export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
   {
@@ -45,9 +38,10 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
         <div>
           <Image
             src={row.original.verification}
-            height={14}
-            width={14}
+            height={16}
+            width={16}
             alt="img"
+            className="w-[16px] h-[16px]"
           />
         </div>
       );
@@ -57,7 +51,7 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
     header: "Profile",
     cell: ({ row }) => {
       return (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-[8px]">
           <div>
             <Image
               src={row.original.image}
@@ -67,14 +61,14 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
               className="rounded-[8px]"
             />
           </div>
-          <div className=" w-[250px] ">
-            <h4 className="text-[18px] text-gradient font-semibold">
+          <div className="w-[250px] flex flex-col items-start">
+            <h4 className="text-lg text-gradient font-semibold leading-[21px]">
               {row.original.name}
             </h4>
-            <h5 className="text-[16px] font-normal text-gradient py-2">
+            <h5 className="text-base font-normal leading-[19px] text-[#3D3D3D] py-2">
               @<span className="text-[#3D3D3D]"> {row.original.userName}</span>
             </h5>
-            <p className="text-[16px] font-normal text-wrap text-gradient">
+            <p className="text-base font-normal text-gradient leading-[19px] text-left">
               {row.original.userStatus}
             </p>
           </div>
@@ -116,7 +110,7 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
     },
   },
   {
-    header: "Admin Fees",
+    header: "Total Fees",
     cell: ({ row }) => {
       return (
         <div>
@@ -128,7 +122,7 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
     },
   },
   {
-    header: "Paid Fees",
+    header: "Due Fees",
     cell: ({ row }) => {
       return (
         <div>
@@ -140,7 +134,7 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
     },
   },
   {
-    header: "Total Fees",
+    header: "Paid Fees",
     cell: ({ row }) => {
       return (
         <div>
@@ -153,33 +147,10 @@ export const StoreListColumn: ColumnDef<DemoTableItemsType>[] = [
   },
   {
     header: "Actions",
-    cell: () => {
+    cell: ({row}) => {
       return (
         <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="ghost"
-                className="h-8 w-8 p-0 focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
-              >
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              align="end"
-              className="bg-white h-auto w-[110px] rounded-lg shadow-[4px_4px_8px_0px_#0000000D,-4px_-4px_8px_0px_#0000000D]"
-            >
-              <DropdownMenuItem className="p-[8px] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0">
-                Edit
-              </DropdownMenuItem>
-              <DropdownMenuItem className="p-[8px] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0">
-                Details
-              </DropdownMenuItem>
-              <DropdownMenuItem className="p-[8px] text-red-600 hover:bg-[#E6EEF6] rounded-b-[8px] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0">
-                Delete
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <StoreAction row={row}/>
         </div>
       );
     },
