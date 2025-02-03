e89bed3b67f5ed93bd5214bb75b1525bda1562cb
sozibhossain <sozibhossain69@mail.com>
2025-02-03 18:22:25 +0600

    Update marketing and promotions upload image

--- src/app/(dashboard)/auction-listing/_components/ProductTogler.tsx
+++ src/app/(dashboard)/auction-listing/_components/ProductTogler.tsx
@@ -1,23 +1,23 @@
-'use client'
+"use client";
 
-import React from 'react'
-import AuctionHeader from './AuctionHeader'
-import AuctionFilter from './AuctionFilter'
-import VendorAuctionContainer from './VendorAuctionContainer'
-import { AddNewProduct } from './AddNewProduct'
+import React from "react";
+import AuctionHeader from "./AuctionHeader";
+import AuctionFilter from "./AuctionFilter";
+import VendorAuctionContainer from "./VendorAuctionContainer";
+import { AddNewProduct } from "./AddNewProduct";
 
 function ProductTogler() {
-    const [showAddProduct, setShowAddProduct] = React.useState(false)
+  const [showAddProduct, setShowAddProduct] = React.useState(false);
   return (
-    <div className='space-y-[30px] mb-[30px]'>
-        <AuctionHeader setShowAddProduct={setShowAddProduct} showAddProduct={showAddProduct}/>
-        <AuctionFilter/>
-        {
-            showAddProduct ? <AddNewProduct/> : <VendorAuctionContainer />
-        }
-       
+    <div className="space-y-[30px] mb-[30px]">
+      <AuctionHeader
+        setShowAddProduct={setShowAddProduct}
+        showAddProduct={showAddProduct}
+      />
+      <AuctionFilter />
+      {showAddProduct ? <AddNewProduct /> : <VendorAuctionContainer />}
     </div>
-  )
+  );
 }
 
-export default ProductTogler
+export default ProductTogler;
