518a1334e6fce9a38e64c5807a37a6c4d38d4a80
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-20 18:18:09 +0600

    membership get api integration

--- src/app/(dashboard)/membership/_components/membership-column.tsx
+++ src/app/(dashboard)/membership/_components/membership-column.tsx
@@ -3,10 +3,10 @@
 import { blurDataUrl } from "@/data/blur-data.url";
 import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
-import { Membership } from "./data";
 import MembershipAction from "./membership-auction";
+import { MembershipPlan } from "@/types/membership";
 
-export const MembershipColumns: ColumnDef<Membership>[] = [
+export const MembershipColumns: ColumnDef<MembershipPlan>[] = [
   {
     header: "Profile",
 
@@ -14,7 +14,7 @@ export const MembershipColumns: ColumnDef<Membership>[] = [
       return (
         <div className="flex items-center gap-[12px] w-fit mx-auto ">
           <Image
-            src={row.original.profileImage}
+            src={row.original.profileImage || "/assets/img/profile_img.png"}
             alt="Profile"
             height={100}
             width={100}
@@ -39,7 +39,7 @@ export const MembershipColumns: ColumnDef<Membership>[] = [
     cell: ({ row }) => {
       return (
         <div className="bg-primary text-white p-[10px] rounded-[12px] ">
-          {row.original.plan}
+          {row.original.planType}
         </div>
       );
     },
@@ -49,7 +49,7 @@ export const MembershipColumns: ColumnDef<Membership>[] = [
     cell: ({ row }) => {
       return (
         <p className="text-[16px] leading-[19.2px] font-normal text-[#444444]">
-          {row.original.time}
+          {row.original.time || "3:00"}
         </p>
       );
     },
@@ -59,8 +59,8 @@ export const MembershipColumns: ColumnDef<Membership>[] = [
     cell: ({ row }) => {
       return (
         <div className="text-[16px] font-normal leading-[19.2px] text-[#444444] space-y-[8px]">
-          <p>Auction: {row.original.auctionRemaining}</p>
-          <p>Bids: {row.original.bidsRemaining}</p>
+          <p>Auction: {row.original.numberOfAuction}</p>
+          <p>Bids: {row.original.numberOfBids}</p>
         </div>
       );
     },
@@ -69,7 +69,7 @@ export const MembershipColumns: ColumnDef<Membership>[] = [
     header: "Store",
     cell: ({ row }) => (
       <p className="text-gradient text-[18px] leading-[21.6px] font-semibold">
-        {row.original.store}
+        {row.original?.store || "No Stote"}
       </p>
     ),
   },
