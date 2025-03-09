96628cf839a1c2292f23064861529937b11c5be6
Kongkon-79 <kongkonjowarder@gmail.com>
2025-03-09 15:16:11 +0600

    category show filtering query integration

--- src/app/(dashboard)/category/_components/CategoryTogler.tsx
+++ src/app/(dashboard)/category/_components/CategoryTogler.tsx
@@ -4,17 +4,21 @@ import CategoryHeader from './CategoryHeader'
 import AddnewCategory from './AddnewCategory'
 import CategoryList from './CategoryList'
 import CategoryFilter from './category-filter'
+import { useIndustryShow } from '@/zustand/features/category/useShowIndustry'
 function CategoryTogler() {
-    const [showcategory, setShowCategory] = React.useState(false)
+    const [showcategory, setShowCategory] = React.useState(false);
+    const { show, setShow } = useIndustryShow();
+
+    
   
 
 
   return (
     <div className='space-y-[30px] mb-[30px]'>
         <CategoryHeader setShowCategory={setShowCategory} showcategory={showcategory}/>
-          <CategoryFilter />
+          <CategoryFilter show={show} setShow={setShow}/>
         {
-        showcategory ? <AddnewCategory setShowCategory={setShowCategory} /> : <CategoryList />
+        showcategory ? <AddnewCategory setShowCategory={setShowCategory} /> : <CategoryList show={show}/>
         }
        
     </div>
--- src/app/(dashboard)/vendor-management/page.tsx
+++ src/app/(dashboard)/vendor-management/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import CustomerListHeader from "./_components/CustomerListHeader";
 import VendorManagementContainer from "./_components/VendorManagementContainer";
-// import CustomerFilter from "./_components/CustomerFilter";
-// import CustomerContainer from "./_components/CustomerContainer";
 
 const page = () => {
   return (
